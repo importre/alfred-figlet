d437558b6d5e3548e851cdca1081f43468b404ae
Jaewe Heo <jaeweheo@gmail.com>
2016-12-21 19:49:54 +0900

    Update package.json & Add notifier

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const alfy = require('alfy');
+const alfredNotifier = require('alfred-notifier');
 const input = alfy.input;
 const font = alfy.config.get('font') || 'Standard';
 const list = [{
@@ -7,5 +8,6 @@ const list = [{
   arg: input,
 }];
 const items = alfy.inputMatches(list, 'title');
+alfredNotifier();
 alfy.output(items);
 
